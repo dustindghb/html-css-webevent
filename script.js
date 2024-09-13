652fbe9176f8b74c24dd98a4faeb527a93420913
dustin duong <ddustinduong@gmail.com>
2024-09-12 23:54:02 -0700

    hamburger icon mobile
    added hamburger icon mobile

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    //targeting these two elements
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
